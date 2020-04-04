cffd4d371e6779fe9be4ed1912406b68562f98bc
arlind.nocaj <arlind.nocaj@gmail.com>
2020-04-04 18:00:38 +0200

    added tessdata download script

---
+++ flask_project/tessdata/download.sh
@@ -0,0 +1,4 @@
+wget https://github.com/tesseract-ocr/tessdata_best/raw/master/deu.traineddata
+wget https://github.com/tesseract-ocr/tessdata_best/raw/master/fra.traineddata
+wget https://github.com/tesseract-ocr/tessdata_best/raw/master/ita.traineddata
+
